9f42c29faad830ca15ae4a2158576f8cddf95229
Mário César Silva <mcgato33@hotmail.com>
2021-05-28 06:34:06 -0100

    model basic type

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -10,16 +10,17 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = {
-    id: uuidv4(),
-    name, 
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
     description,
     created_at: new Date(),
-  }
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
