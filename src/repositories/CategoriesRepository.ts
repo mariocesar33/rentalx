5841ca3f1eb8903606ed7eb4a384e25d33b0fdac
Mário César Silva <mcgato33@hotmail.com>
2021-05-28 16:11:27 -0100

    Create CategoriesRepository.ts

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,29 @@
+import { Category } from "../model/Category";
+
+// DTO (Data Transfer Object)
+interface ICreatecategoryDTO {
+  name: string,
+  description: string,
+}
+
+class categoriesRepository {
+  private categories: Category[];
+
+  constructor () {
+    this.categories = [];
+  }
+
+  create({ name, description}: ICreatecategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { categoriesRepository }
