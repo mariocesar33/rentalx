4de47ec984357983a44a85cc8f48a753565ab7d9
Mário César Silva <mcgato33@hotmail.com>
2021-05-29 05:47:38 -0100

    validando cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Categoria já existe!"});
+  }
+
   categoriesRepository.create({
     name,
     description,
