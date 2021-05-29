430b7f8c0580c6c8fa3303f3753e02a12c00d7d5
Mário César Silva <mcgato33@hotmail.com>
2021-05-29 18:38:44 -0100

    Update CreateCategoryService.ts

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,32 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * Definir o tipo de retorno
+ * alterar o retorno de erro
+ * acessar o repositório
+ */
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {
+
+  }
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Categoria já existe!");
+    }
+
+    this.categoriesRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateCategoryService }
