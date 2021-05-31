f3b6a1591c1c47f2784e8555008ee7096c752fe5
Mário César Silva <mcgato33@hotmail.com>
2021-05-31 18:54:56 -0100

    Update ListCategoriesController.ts

--- src/modules/cars/useCases/listCategory/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategory/ListCategoriesController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 
+import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
+  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+
   handle(request: Request, response: Response): Response {
-    const all = categoriesRepository.list();
+    const all = this.listCategoriesUseCase.execute();
 
     return response.json(all);
   }
