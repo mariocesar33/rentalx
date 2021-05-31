3361e1c594ae146539c3de1661088e3301a0b1f9
Mário César Silva <mcgato33@hotmail.com>
2021-05-31 18:50:22 -0100

    Update ListCategoriesUseCase.ts

--- src/modules/cars/useCases/listCategory/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategory/ListCategoriesUseCase.ts
@@ -1,6 +1,13 @@
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
 class ListCategoriesUseCase {
-  execute(): void {
-    
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
   }
 }
 
