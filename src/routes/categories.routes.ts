2047d07599b90c95e9f2f70f808e33be875168d2
Mário César Silva <mcgato33@hotmail.com>
2021-07-23 12:13:16 -0100

    Usando docker-compose

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
+  console.log("reload 000 funcionando")
   return createCategoryController.handle(request, response);
 });
 
