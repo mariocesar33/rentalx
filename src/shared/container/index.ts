fd62dd82f4387ca6dbfafc61cc594ea380939d9a
Mário César Silva <mcgato33@hotmail.com>
2021-08-16 19:03:38 -0100

    Update index.ts

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,10 @@
 import { container } from "tsyringe";
+
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+
+// ICategoriesRepositoy
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository
+)
