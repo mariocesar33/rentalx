8f0d874b2c79dcbb9f50239a1ae87a3061c199f3
Mário César Silva <mcgato33@hotmail.com>
2021-08-18 19:20:56 -0100

    Update CreateSpecificationController.ts

--- src/modules/cars/useCases/createSpecificationController/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecificationController/CreateSpecificationController.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
 
-  async handle(request: Request, response: Response): Promise<Response> {
+  handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
+
+    const createSpecificationUseCase = container.resolve(
+      CreateSpecificationUseCase
+    );
     
-    await this.createSpecificationUseCase.execute({ name, description });
+    createSpecificationUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
