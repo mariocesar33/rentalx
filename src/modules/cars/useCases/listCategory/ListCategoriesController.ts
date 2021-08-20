34c3a539255794ffb05fc0f31f18eafecc67a57b
Mário César Silva <mcgato33@hotmail.com>
2021-08-20 06:07:26 -0100

    Update ListCategoriesController.ts

--- src/modules/cars/useCases/listCategory/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategory/ListCategoriesController.ts
@@ -6,7 +6,7 @@ import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 class ListCategoriesController {
 
   async handle(request: Request, response: Response): Promise<Response> {
-    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase)
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
     const all = await listCategoriesUseCase.execute();
 
     return response.json(all);
