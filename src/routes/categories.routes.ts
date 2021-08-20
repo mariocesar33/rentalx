64b17722703b1e0ec66b30c27ae61fd6b0dbebe4
Mário César Silva <mcgato33@hotmail.com>
2021-08-20 06:15:34 -0100

    Update categories.routes.ts

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
-import { listCategoriesController } from "../modules/cars/useCases/listCategory";
+import { ListCategoriesController } from "../modules/cars/useCases/listCategory/ListCategoriesController";
 import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController"
 
 const categoriesRoutes = Router();
@@ -12,13 +12,12 @@ const upload = multer({
 });
 
 const createCategoryController = new CreateCategoryController();
+const listCategoriesController = new ListCategoriesController();
 const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
-categoriesRoutes.get("/", (request, response) => {
-  return listCategoriesController.handle(request, response);
-});
+categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post("/import", upload.single("file"), importCategoryController.hundle);
 
